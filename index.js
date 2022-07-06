ee973d94bf5a8c1619edc7b004d522dd06acb348
rachidelaid <rachidelaid@gmail.com>
2022-07-06 12:47:45 +0100

    changing svelte dist folder and installing express

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
