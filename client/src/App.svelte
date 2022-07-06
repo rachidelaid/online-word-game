8be5e8b338f7afcc756da3cd60f06e8333c0313e
rachidelaid <rachidelaid@gmail.com>
2022-07-06 13:24:58 +0100

    home page is done

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,65 +1,38 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import Home from './views/Home.svelte';
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <Home />
 </main>
 
 <style>
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    --color-primary: #0070f3;
+    --color-secondary: #3593ff;
+    --color-success: #00b300;
+    --color-danger: #ff0000;
+    --color-warning: #ff9100;
+    --color-info: #474747;
   }
 
   main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    padding: 2rem;
   }
 </style>
---
+++ client/src/views/Home.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { fade } from 'svelte/transition';
+  let active = 'Create';
+
+  const toggleActive = (e) => {
+    active = e.target.textContent.trim();
+  };
+</script>
+
+<div class="home">
+  <div class="head" on:click={toggleActive}>
+    <p class={active === 'Create' ? 'active' : ''}>Create</p>
+    <p class={active === 'Join' ? 'active' : ''}>Join</p>
+  </div>
+
+  {#if active === 'Create'}
+    <div class="create" in:fade>
+      <h1>Create</h1>
+      <p>Create a new game and invite your friends to join.</p>
+      <input type="text" placeholder="Your Name" />
+      <input type="text" placeholder="Room Id" />
+      <button>Create</button>
+    </div>
+  {:else}
+    <div class="join" in:fade>
+      <h1>Join</h1>
+      <p>Join a game that has been created.</p>
+      <input type="text" placeholder="Your Name" />
+      <input type="text" placeholder="Room Id" />
+      <button>Join</button>
+    </div>
+  {/if}
+</div>
+
+<style>
+  .home {
+    width: 20rem;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+
+  .home .head {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .home .head p {
+    cursor: pointer;
+    transition: 0.3s ease-in-out;
+  }
+
+  .home .head p:hover {
+    color: var(--color-primary);
+  }
+
+  .home .head p.active {
+    border-bottom: 2px solid var(--color-primary);
+  }
+</style>
